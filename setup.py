584f069444086fb43e112591c7b9a66325e5660d
Sascha Vowe <vowe@mail.de>
2020-03-26 00:53:01 +0100

    required packages added

--- setup.py
+++ setup.py
@@ -5,18 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="talisestools",
-    version="0.0.3",
+    version="0.0.5",
     author="Sascha Vowe",
     author_email="vowe@mail.de",
     description="Tools for handling TAELISES generated binary data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/savowe/talisestools",
+    install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
     packages=['talisestools'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.1',
 )
