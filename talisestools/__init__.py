02b3b54dd0665900570f5aca24ffbc3d559766bd
Sascha Vowe <vowe@mail.de>
2020-03-27 17:21:21 +0100

    2D plot functionality added

--- talisestools/__init__.py
+++ talisestools/__init__.py
@@ -70,21 +70,21 @@ def readbin(input_files, save=False):
                 data[i,n] = complex(cmplxno[0],cmplxno[1])
         if save == True:
             import scipy.io as sio
-            sio.savemat(newfilename, mdict={'wavefunction': data, 'nDimX': nDimX, 'xMin': xMin, 'xMax': xMax, 'dx': dx, 'L': L, 'T': T, 't': t} )
+            sio.savemat(newfilename, mdict={'wavefunction': data, 'nDims': nDims, 'nDimX': nDimX, 'xMin': xMin, 'xMax': xMax, 'dx': dx, 'L': L, 'T': T, 't': t} )
             print(newfilename+" created.")
 
-        return {'wavefunction': data, 'nDimX': nDimX, 'xMin': xMin, 'xMax': xMax, 'dx': dx, 'L': L, 'T': T, 't': t}
+        return {'wavefunction': data, 'nDims': nDims, 'nDimX': nDimX, 'xMin': xMin, 'xMax': xMax, 'dx': dx, 'L': L, 'T': T, 't': t}
 
     if (nDims == 2):
-        Nt = int(data_length/(header[0]+nDimX*DimY*cmplxsize)) #Number of time-steps in file
+        Nt = int(data_length/(header[0]+nDimX*nDimY*cmplxsize)) #Number of time-steps in file
         data = zeros((nDimX,nDimY),dtype=complex_) 
         t = zeros(Nt)
         for n in range(0,Nt):
-            fh.seek(n*(header[0]+nDimX*DimY*cmplxsize)) # go to beginning of file
+            fh.seek(n*(header[0]+nDimX*nDimY*cmplxsize)) # go to beginning of file
             header_raw = fh.read(calcsize("llllllliiddddddddddddddiidd"))
             header = unpack( "llllllliiddddddddddddddiidd", header_raw )
             t[n] = header[9]
-            fh.seek(n*(header[0]+nDimX*DimY*cmplxsize))
+            fh.seek(n*(header[0]+nDimX*nDimY*cmplxsize))
             fh.seek(header[0], 1) #skip header file relative to above position
             for i in range(0, nDimX):
                 for j in range(0, nDimY):
@@ -97,9 +97,9 @@ def readbin(input_files, save=False):
             print("yrange = (%g,%g)" % (yMin,yMax))
             print("t = %g" % (t))
             import scipy.io as sio
-            sio.savemat(newfilename, mdict={'wavefunction': data, 'nDimX': nDimX, 'nDimY': nDimY, 'xMin': xMin, 'yMin': yMin, 'xMax': xMax, 'yMax': yMax, 'dx': dx, 'dy': dy, 'L': L, 'T': T, 't': t} )
+            sio.savemat(newfilename, mdict={'wavefunction': data, 'nDims': nDims, 'nDimX': nDimX, 'nDimY': nDimY, 'xMin': xMin, 'yMin': yMin, 'xMax': xMax, 'yMax': yMax, 'dx': dx, 'dy': dy, 'L': L, 'T': T, 't': t} )
             print(newfilename+" created.")
-        return {'wavefunction': data, 'nDimX': nDimX, 'nDimY': nDimY, 'xMin': xMin, 'yMin': yMin, 'xMax': xMax, 'yMax': yMax, 'dx': dx, 'dy': dy, 'L': L, 'T': T, 't': t}
+        return {'wavefunction': data, 'nDims': nDims, 'nDimX': nDimX, 'nDimY': nDimY, 'xMin': xMin, 'yMin': yMin, 'xMax': xMax, 'yMax': yMax, 'dx': dx, 'dy': dy, 'L': L, 'T': T, 't': t}
             
         
     fh.close()
@@ -107,7 +107,7 @@ def readbin(input_files, save=False):
 
 def plotbin(filename):
     data = readbin(filename)
-    if len(data["wavefunction"].shape)==2:
+    if data["nDims"]==1:
         x = arange(data["xMin"], data["xMax"], data["dx"])
         psi2 = abs(power(data["wavefunction"][:,0],2))
         plt.ylabel(r"$|\Psi |^2$")
@@ -121,10 +121,10 @@ def plotbin(filename):
         print(filename+".png has been created.")
         plt.close()
 
-    if len(data["wavefunction"].shape)==3:
+    if data["nDims"]==2:
         x = arange(data["xMin"], data["xMax"], data["dx"])
         y = arange(data["yMin"], data["yMax"], data["dy"])
-        psi2= abs(np.power(data["wavefunction"][:,:,0],2))
+        psi2= abs(power(data["wavefunction"][:,:],2))
         plt.title(r"$|\Psi |^2$ at t="+str(data["t"][0]))
         plt.ylabel("y")
         plt.xlabel("x")
